45275f60b76492b0443a1f649c3b972eae52bc9f
Sankalp Garg <sankalp@optexity.com>
2026-03-15 00:08:44 -0700

    detached mode script

--- images/chromium-headful/run-docker.sh
+++ images/chromium-headful/run-docker.sh
@@ -86,4 +86,9 @@ if [[ "${ENABLE_WEBRTC:-}" == "true" ]]; then
 fi
 
 docker rm -f "$NAME" 2>/dev/null || true
-docker run -it "${RUN_ARGS[@]}" "$IMAGE"
+
+if [[ "${1:-}" == "-it" ]]; then
+  docker run -it "${RUN_ARGS[@]}" "$IMAGE"
+else
+  docker run -d --rm "${RUN_ARGS[@]}" "$IMAGE"
+fi
